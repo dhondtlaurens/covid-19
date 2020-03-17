6b8f77d7eee210ac38fef04064c9823917a7d5d0
Laurens D'Hondt <laurens@laurensdhondt.be>
2020-03-17 18:11:34 +0100

    changed NL copy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="home px-16 pt-32 pb-64 ">
     <div class="row flex flex-wrap">
-      <div class="w-full lg:w-1/3 px-16 pb-32 text-blue-300">
+      <div class="w-full lg:w-1/2 px-16 pb-32 text-blue-300">
         <div
           class="mb-16 text-24 font-medium"
           v-html="$t('views.home.title')"
         ></div>
 
-        <div class="text-16">
-          {{ $t('views.home.description') }}
-        </div>
+        <div
+          class="text-16"
+          v-html="$t('views.home.description')"
+        ></div>
       </div>
 
-      <div class="flex flex-wrap w-full lg:w-2/3">
-        <div class="flex-shrink-0 px-16 w-full md:w-1/2 xl:w-1/4 mb-32">
+      <div class="flex flex-wrap w-full lg:w-1/2">
+        <div class="flex-shrink-0 px-16 w-full md:w-1/2 mb-32">
           <div class="border border-gray-100">
             <div class="flex items-center px-16 h-32 text-blue-300 font-medium text-16 border-b border-gray-100">
               {{ $t('views.home.cases') }}
@@ -25,7 +26,7 @@
           </div>
         </div>
 
-        <div class="flex-shrink-0 px-16 w-full md:w-1/2 xl:w-1/4 mb-32">
+        <div class="flex-shrink-0 px-16 w-full md:w-1/2 mb-32">
           <div class="border border-gray-100">
             <div class="flex items-center px-16 h-32 text-blue-300 font-medium text-16 border-b border-gray-100">
               {{ $t('views.home.deaths') }}
@@ -37,7 +38,7 @@
           </div>
         </div>
 
-        <div class="flex-shrink-0 px-16 w-full md:w-1/2 xl:w-1/4 mb-32">
+        <div class="flex-shrink-0 px-16 w-full md:w-1/2 mb-32">
           <div class="border border-gray-100">
             <div class="flex items-center px-16 h-32 text-blue-300 font-medium text-16 border-b border-gray-100">
               {{ $t('views.home.critical') }}
@@ -49,7 +50,7 @@
           </div>
         </div>
 
-        <div class="flex-shrink-0 px-16 w-full md:w-1/2 xl:w-1/4 mb-32">
+        <div class="flex-shrink-0 px-16 w-full md:w-1/2 mb-32">
           <div class="border border-gray-100">
             <div class="flex items-center px-16 h-32 text-blue-300 font-medium text-16 border-b border-gray-100">
               {{ $t('views.home.recovered') }}
